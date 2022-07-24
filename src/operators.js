eb372a3ffd32bba64591855fae39a89ae45bb171
SkillGG <skilletssgames@gmail.com>
2022-07-24 14:59:19 +0200

    Move images to png

---
+++ src/operators.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+/**
+ * @typedef {{name:string, stars:number, tags:string[], image:string}} Operator
+ */
+
+/**
+ * @typedef {Operator[]} Operators
+ */
+
+/**
+ * @type {Operators}
+ */
+const operatorJSON = JSON.parse(fs.readFileSync("./operators.json").toString());
+
+const newOperatorJSON = operatorJSON.map((char, i) => {
+  //   if (!fs.existsSync("./operators/")) {
+  //     fs.mkdirSync("./operators/");
+  //   }
+  //   const buff = Buffer.from(
+  //     char.image.substring(char.image.indexOf(",")),
+  //     "base64"
+  //   );
+  //   const path = `./operators/${char.name.replace(/\s/g, "_")}.png`;
+  //   fs.writeFileSync(path, buff);
+  //   return { ...char, image: path };
+  //   return {...char, tags: [...new Set(char.tags)]};
+  return char;
+});
+
+newOperatorJSON.sort((p, n) => {
+  const a = p.name.charAt(0);
+  const b = n.name.charAt(0);
+  const lowA = a.toLowerCase();
+  const lowB = b.toLowerCase();
+  if (lowA < lowB) return -1;
+  if (lowA > lowB) return 1;
+  return 0;
+});
+
+// console.log(newOperatorJSON.map((r) => r.tags));
+
+fs.writeFileSync("./operators.json", JSON.stringify(newOperatorJSON));
