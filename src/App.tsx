d2386b0b21bfdc482aefc9acaa62994c0024e116
SkillGG <skilletssgames@gmail.com>
2022-11-13 13:30:27 +0100

    Move Recruitment Pages together to refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,158 +1,86 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import CharacterShow from "./Recruitment/Picker/CharacterShow";
-import FilterPicker from "./Recruitment/Picker/FilterPicker";
 import PityTracker from "./Pity/PityTracker";
 import {
   ArkData,
   checkIfIsASetting,
   DEFAULT_SETTINGS,
-  Filter,
-  FullRecruitmentTags,
-  getFilters,
   PageType,
   parseLocation,
-  PastRecruitment,
   Settings,
   ShallowCopy,
-  toggleTag,
 } from "./utils";
 
-import Chars from "./operators.json";
-import NavBar from "./NavBar";
+/**
+ * All characters data from json (untyped)
+ */
+import untypedChars from "./operators.json";
+/**
+ * All localStorage IDs
+ */
 import StorageIDS from "./localStorageIDs.json";
-import RecruitmentHistory from "./Recruitment/History/RecruitmentHistory";
-import {
-  HistoryUpdate,
-  isFullHistoryUpdate,
-  RecHis,
-} from "./Recruitment/History/rechis.util";
+
+import NavBar from "./NavBar";
+import RecruitmentPage from "./Recruitment/Recruitment";
 
 function App() {
-  const [filters, setFilters] = useState<Filter[]>([]);
-  const [characters] = useState<ArkData[]>(Chars);
-  const [tag, setTag] = useState(true);
+  /**
+   * All character data from json (typed)
+   */
+  const characters: ArkData[] = untypedChars;
 
+  /**
+   * Switch what page are we on
+   */
   const [page, setPage] = useState<PageType>("recruit");
 
-  const [firstLoad, setFirstLoad] = useState(false);
-
+  /**
+   * Site settings
+   */
   const [settings, setSettings] = useState<Settings>(
     JSON.parse(localStorage.getItem(StorageIDS.settings) || DEFAULT_SETTINGS)
   );
 
-  const [recHistory, setRecHistory] = useState<PastRecruitment[]>(
-    RecHis.decompress(
-      localStorage.getItem(StorageIDS.recruitment.history) || "[]"
-    ).filter((z: any) => z)
-  );
+  /**
+   * Flag that changes when onpopstate has bee triggered to relay that information to recruitment filters to update based on url
+   */
+  const [popState, setPopState] = useState(false);
 
+  /**
+   * Changes a given setting
+   * @param setting Setting to change
+   * @param value new value for a setting
+   */
   const changeSetting = (setting: string, value: any) => {
     if (checkIfIsASetting(setting)) settings[setting] = value;
     localStorage.setItem(StorageIDS.settings, JSON.stringify(settings));
     setSettings(ShallowCopy(settings));
   };
 
-  const refreshURLHistory = () => {
-    history.pushState(
-      "",
-      "",
-      `?filter=${filters
-        .filter((f) => f.id)
-        .map((f) => f.id)
-        .join(",")}`
-    );
-  };
-
-  const isSelected = (s: string) => {
-    return !!filters.find((f) => f.id === s);
-  };
-  const tooMany = (s: string[]) => s.length > 5;
-  const select = (s: string) => {
-    const curr = toggleTag(
-      filters.map((f) => f.id),
-      s
-    ).filter((t) => !!t);
-    if (tooMany(curr)) return;
-    setFilters(getFilters(curr));
-  };
-  const selectMany = (sarr: string[]) => {
-    const checked = sarr.slice(0, 5);
-    setFilters(getFilters(checked));
-  };
-
+  /**
+   * Set page based on url
+   */
   useEffect(() => {
     const locationData = parseLocation();
     setPage(locationData.path);
-    if (locationData.path === "recruit") {
-      selectMany(locationData.filters);
-    }
-    setFirstLoad(true);
   }, []);
 
+  /**
+   * Set page on back button
+   */
   window.onpopstate = () => {
     const locationData = parseLocation();
     setPage(locationData.path);
-    if (locationData.path === "recruit") {
-      selectMany(locationData.filters);
-    }
+    setPopState((popState) => !popState);
   };
 
-  useEffect(() => {
-    if (firstLoad || filters.length > 0) {
-      refreshURLHistory();
-    }
-  }, [filters]);
-
+  /**
+   * Change page
+   * @param page What page to change to
+   */
   const moveToPage = (page: PageType) => {
     history.pushState(null, "", page);
     setPage(page);
-  };
-
-  const addToRecHistory = (
-    ids: FullRecruitmentTags,
-    picked: string[],
-    selected?: ArkData
-  ) => {
-    saveHistoryChangeToLS({
-      f: (rh: PastRecruitment[]) => {
-        rh.push({
-          tags: ids.filter((f) => !!f),
-          date: new Date().getTime(),
-          outcome: selected ? selected.name : undefined,
-          picked,
-        });
-        return rh;
-      },
-    });
-  };
-
-  const tagsRefreshed = () => {
-    addToRecHistory(
-      filters.map((r) => r.id),
-      []
-    );
-    setFilters([]);
-  };
-
-  const saveHistoryChangeToLS = (o: HistoryUpdate) => {
-    const useCompression = (sh: PastRecruitment[]) => {
-      const JSONstr = JSON.stringify(sh);
-      // console.log(JSONstr);
-      const RECHstr = RecHis.compress(sh);
-      return RECHstr;
-    };
-    let srh = [...recHistory];
-    if (!isFullHistoryUpdate(o)) {
-      const index = recHistory.findIndex((rh) => rh.date === o.d);
-      if (index !== -1) {
-        srh[index] = o.f(srh[index]);
-      }
-    } else if (isFullHistoryUpdate(o)) {
-      srh = o.f(srh);
-    }
-    localStorage.setItem(StorageIDS.recruitment.history, useCompression(srh));
-    setRecHistory(srh);
   };
 
   return (
@@ -170,71 +98,15 @@ function App() {
         changeSetting={changeSetting}
         settings={settings}
       />
-      {page === "recruit" ? (
-        <>
-          <FilterPicker
-            select={select}
-            isSelected={isSelected}
-            unselectAll={() => setFilters([])}
-            tagsRefreshed={tagsRefreshed}
-            tag={tag}
-            setTag={setTag}
-            settings={settings}
-          />
-          <CharacterShow
-            characters={characters}
-            filters={filters}
-            showTag={tag}
-            selectRecruitment={(
-              s: string[],
-              p: string[],
-              selected?: ArkData
-            ) => {
-              addToRecHistory(s, p, selected);
-              setFilters([]);
-            }}
-            settings={settings}
-          />
-        </>
-      ) : page === "pity" ? (
+      {page === "pity" ? (
         <PityTracker settings={settings} />
       ) : (
-        <>
-          <RecruitmentHistory
-            settings={settings}
-            characters={characters}
-            recHistory={recHistory}
-            toggleStrikeOut={(d, t) => {
-              saveHistoryChangeToLS({
-                d,
-                f: (rh: PastRecruitment) => {
-                  const tagIndex = rh.picked.findIndex((tag) => tag === t);
-                  rh.picked[tagIndex] =
-                    t.charAt(0) === "-" ? t.substring(1) : `-${t}`;
-                  return rh;
-                },
-              });
-            }}
-            setOutcome={(d, o) => {
-              saveHistoryChangeToLS({
-                d,
-                f: (rh: PastRecruitment) => {
-                  rh.outcome = o.name;
-                  return rh;
-                },
-              });
-            }}
-            removeFromHistory={(d) => {
-              saveHistoryChangeToLS({
-                d,
-                f: (rh: PastRecruitment) => {
-                  rh = { date: 0, picked: [], tags: [] };
-                  return rh;
-                },
-              });
-            }}
-          />
-        </>
+        <RecruitmentPage
+          characters={characters}
+          settings={settings}
+          page={page}
+          popped={popState}
+        />
       )}
     </>
   );
---
+++ src/Recruitment/Recruitment.tsx
@@ -0,0 +1,236 @@
+import React, { FunctionComponent, useEffect, useState } from "react";
+import {
+  ArkData,
+  Filter,
+  FullRecruitmentTags,
+  getFilters,
+  PageType,
+  parseLocation,
+  PastRecruitment,
+  Settings,
+  toggleTag,
+} from "../utils";
+import {
+  HistoryUpdate,
+  isFullHistoryUpdate,
+  RecHis,
+} from "./History/rechis.util";
+import CharacterShow from "./Picker/CharacterShow";
+import FilterPicker from "./Picker/FilterPicker";
+
+import StorageIDS from "./../localStorageIDs.json";
+import RecruitmentHistory from "./History/RecruitmentHistory";
+
+interface RecruitmentPageProps {
+  /**
+   * All character data
+   */
+  characters: ArkData[];
+  /**
+   * Flag which page to show
+   */
+  page: Exclude<PageType, "pity">;
+  /**
+   * Page settings
+   */
+  settings: Settings;
+  /**
+   * Flag that triggers onpopstate useEffect
+   */
+  popped: boolean;
+}
+
+const RecruitmentPage: FunctionComponent<RecruitmentPageProps> = ({
+  characters,
+  page,
+  settings,
+  popped,
+}) => {
+  const [tag, setTag] = useState(true);
+
+  /**
+   * Chosen filters
+   */
+  const [filters, setFilters] = useState<Filter[]>([]);
+
+  const [firstLoad, setFirstLoad] = useState(false);
+
+  const [recHistory, setRecHistory] = useState<PastRecruitment[]>(
+    RecHis.decompress(
+      localStorage.getItem(StorageIDS.recruitment.history) || "[]"
+    ).filter((z: any) => z)
+  );
+
+  useEffect(() => {
+    const locationData = parseLocation();
+    if (locationData.path === "recruit") {
+      selectMany(locationData.filters);
+    }
+    setFirstLoad(true);
+  }, []);
+
+  /**
+   * window.onpopstate
+   */
+  useEffect(() => {
+    const locationData = parseLocation();
+    if (locationData.path === "recruit") {
+      selectMany(locationData.filters);
+    }
+  }, [popped]);
+
+  const refreshURLHistory = () => {
+    history.pushState(
+      "",
+      "",
+      `?filter=${filters
+        .filter((f) => f.id)
+        .map((f) => f.id)
+        .join(",")}`
+    );
+  };
+
+  const isSelected = (s: string) => {
+    return !!filters.find((f) => f.id === s);
+  };
+  const tooMany = (s: string[]) => s.length > 5;
+  const select = (s: string) => {
+    const curr = toggleTag(
+      filters.map((f) => f.id),
+      s
+    ).filter((t) => !!t);
+    if (tooMany(curr)) return;
+    setFilters(getFilters(curr));
+  };
+  const selectMany = (sarr: string[]) => {
+    const checked = sarr.slice(0, 5);
+    setFilters(getFilters(checked));
+  };
+
+  useEffect(() => {
+    const locationData = parseLocation();
+    if (locationData.path !== "recruit") return () => {};
+    if (firstLoad || filters.length > 0) {
+      if (locationData.filters.length !== filters.filter((f) => f.id).length)
+        refreshURLHistory();
+    }
+  }, [filters]);
+
+  const saveHistoryChangeToLS = (o: HistoryUpdate) => {
+    const useCompression = (sh: PastRecruitment[]) => {
+      const JSONstr = JSON.stringify(sh);
+      // console.log(JSONstr);
+      const RECHstr = RecHis.compress(sh);
+      return RECHstr;
+    };
+    let srh = [...recHistory];
+    if (!isFullHistoryUpdate(o)) {
+      const index = recHistory.findIndex((rh) => rh.date === o.d);
+      if (index !== -1) {
+        srh[index] = o.f(srh[index]);
+      }
+    } else if (isFullHistoryUpdate(o)) {
+      srh = o.f(srh);
+    }
+    localStorage.setItem(StorageIDS.recruitment.history, useCompression(srh));
+    setRecHistory(srh);
+  };
+
+  const addToRecHistory = (
+    ids: FullRecruitmentTags,
+    picked: string[],
+    selected?: ArkData
+  ) => {
+    saveHistoryChangeToLS({
+      f: (rh: PastRecruitment[]) => {
+        rh.push({
+          tags: ids.filter((f) => !!f),
+          date: new Date().getTime(),
+          outcome: selected ? selected.name : undefined,
+          picked,
+        });
+        return rh;
+      },
+    });
+  };
+
+  const tagsRefreshed = () => {
+    addToRecHistory(
+      filters.map((r) => r.id),
+      []
+    );
+    setFilters([]);
+  };
+
+  return (
+    <>
+      {page === "recruit" && (
+        <>
+          <FilterPicker
+            select={select}
+            isSelected={isSelected}
+            unselectAll={() => setFilters([])}
+            tagsRefreshed={tagsRefreshed}
+            tag={tag}
+            setTag={setTag}
+            settings={settings}
+          />
+          <CharacterShow
+            characters={characters}
+            filters={filters}
+            showTag={tag}
+            selectRecruitment={(
+              s: string[],
+              p: string[],
+              selected?: ArkData
+            ) => {
+              addToRecHistory(s, p, selected);
+              setFilters([]);
+            }}
+            settings={settings}
+          />
+        </>
+      )}
+      {page === "recHis" && (
+        <>
+          <RecruitmentHistory
+            settings={settings}
+            characters={characters}
+            recHistory={recHistory}
+            toggleStrikeOut={(d, t) => {
+              saveHistoryChangeToLS({
+                d,
+                f: (rh: PastRecruitment) => {
+                  const tagIndex = rh.picked.findIndex((tag) => tag === t);
+                  rh.picked[tagIndex] =
+                    t.charAt(0) === "-" ? t.substring(1) : `-${t}`;
+                  return rh;
+                },
+              });
+            }}
+            setOutcome={(d, o) => {
+              saveHistoryChangeToLS({
+                d,
+                f: (rh: PastRecruitment) => {
+                  rh.outcome = o.name;
+                  return rh;
+                },
+              });
+            }}
+            removeFromHistory={(d) => {
+              saveHistoryChangeToLS({
+                d,
+                f: (rh: PastRecruitment) => {
+                  rh = { date: 0, picked: [], tags: [] };
+                  return rh;
+                },
+              });
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default RecruitmentPage;
